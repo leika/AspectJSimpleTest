5cf61e33c82078f04abad53cd90cdf83ccd8e8a5
Stefan Marx <smarx@apmcentral.org>
2013-06-20 08:04:51 +0200

    initial Commit

---
+++ src/net/andrewewhite/examples/Test.java
@@ -0,0 +1,11 @@
+package net.andrewewhite.examples;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: smarx
+ * Date: 20.06.13
+ * Time: 08:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
